62aade835418254b209e67edd8b09c3ee40d8f7b
Brijesh Chauhan <brijeshch80580@gmail.com>
2023-11-19 16:40:36 +0530

    more patterns added

---
+++ patterns/InvertedHalfNumberPyramid.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InvertedHalfNumberPyramid {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 0; i<n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
