ec7f2ded21ac1ffe7b53275565f778f94bc8687a
Brijesh Chauhan <brijeshch80580@gmail.com>
2023-11-20 14:44:56 +0530

    prefixSum

--- array/MaxSubArraySum.java
+++ array/MaxSubArraySum.java
@@ -9,23 +9,64 @@ public class MaxSubArraySum {
     public static void subarr(int arr[]){
         int f = 0;
         int l = arr.length - 1;
+        int maxSum = Integer.MIN_VALUE;
 
         for (int i = 0; i <= l; i++) {
+            int start = i;
             for (int j = i; j <= l; j++) {
-                for (int k = i; k <= j; k++) {
-                    System.out.print(arr[k]);
+                int end = j;
+                int sum = 0;
+                for (int k = start; k <= end; k++) {
+                    sum = sum + arr[k];
+                    System.out.print(arr[k]); //no
                 }
-                System.out.println();
+                if (maxSum < sum) {
+                    maxSum = sum;
+                }
+                System.out.println("= "+ sum); //set
             }
-            System.out.println();
+            System.out.println(); //line
         }
+
+        System.out.println("Max Sum is: " + maxSum);
 
     }
 
+    //PrefixSum
+    public static void prefixSum (int arr[]) {
+        int f = 0;
+        int l = arr.length - 1;
+        int maxSum = Integer.MIN_VALUE;
+
+        int prefix[] = new int[l+1];
+
+        prefix[0] = arr[0];
+        for (int i = 1; i < l; i++){
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+
+        for (int i = 0; i <= l; i++) {
+            int start = i;
+            for (int j = i; j <= l; j++) {
+                int end = j;
+                int sum = start == 0 ? prefix[end] : prefix[end] - prefix[start -1];
+                
+                if (maxSum < sum) {
+                    maxSum = sum;
+                }
+                System.out.println("= "+ sum); //set
+            }
+            System.out.println(); //line
+        }
+
+        System.out.println("Max Sum is: " + maxSum);
+    }
+
     public static void main(String args[]) {
-        int arr[] = {2, 5, 8, 9, 15, 18};
+        int arr[] = {2, -5, 8, 9, -15, -18};
 
         print(arr);
         subarr(arr);
+        //prefixSum(arr);
     }
 }
